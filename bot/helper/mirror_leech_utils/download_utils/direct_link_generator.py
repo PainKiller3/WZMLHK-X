abd73d871c0add0280118b95afdc66aa947bf405
mohammed ahmed <64513301+mohammedahmed18@users.noreply.github.com>
2026-07-08 21:57:21 +0300

    fix: mediafile file_details request
    Signed-off-by: mohammed ahmed <64513301+mohammedahmed18@users.noreply.github.com>

--- bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
@@ -526,7 +526,7 @@ def mediafile(url):
         postid = postvalue[1].replace("(", "").replace(")", "")
         response = post(
             "https://mediafile.cc/account/ajax/file_details",
-            data={"u": postid},
+            data={"u": postid, "p": "true"},
             headers={"X-Requested-With": "XMLHttpRequest"},
         )
         html = response.json()["html"]
