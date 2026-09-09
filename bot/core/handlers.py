a59d073a4fc3f7921a64c32497efebef143c65ea
PainKiller3 <ninadpatil100@gmail.com>
2026-09-09 19:47:52 +0530

    feat(stats): restrict stats command to owner and sudo users

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -441,7 +441,7 @@ async def add_handlers():
         MessageHandler(
             bot_stats,
             filters=command(BotCommands.StatsCommand, case_sensitive=True)
-            & CustomFilters.authorized,
+            & CustomFilters.sudo,
         )
     )
     TgClient.bot.add_handler(
